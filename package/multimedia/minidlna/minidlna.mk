57fa0539ed265874d1dd7777f76ba9250f51a049
hankhank <andrew@sideramota.com>
2012-04-07 17:26:54 +1000

    Adding minidlna configuration and init scripts

--- package/multimedia/minidlna/minidlna.mk
+++ package/multimedia/minidlna/minidlna.mk
@@ -9,11 +9,13 @@ MINIDLNA_SOURCE = minidlna_$(MINIDLNA_VERSION)_src.tar.gz
 MINIDLNA_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/minidlna
 MINIDLNA_DEPENDENCIES = libexif libid3tag jpeg flac libvorbis sqlite util-linux libav
 MINIDLAN_INSTALL_STAGING = yes
-#libavformat 
 
 define MINIDLNA_BUILD_CMDS
  $(MAKE) STAGING_DIR="$(STAGING_DIR)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
 endef
 
+define MINIDLNA_INSTALL_TARGET_CMDS
+   $(MAKE) -C $(@D) STAGING_DIR="$(TARGET_DIR)" DESTDIR=$(TARGET_DIR) install
+endef
 
 $(eval $(call GENTARGETS,package,minidlna))
